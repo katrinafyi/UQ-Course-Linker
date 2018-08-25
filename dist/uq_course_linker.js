c3f87b806610b7e10971c49ae1c5683b8edccfc8
Kenton Lam <kenton_lam@outlook.com>
2018-08-25 18:13:45 +1000

    Initial commit.

---
+++ dist/uq_course_linker.js
@@ -0,0 +1,5 @@
+function test() {
+    return "this";
+}
+console.log(test());
+//# sourceMappingURL=uq_course_linker.js.map
---
+++ src/uq_course_linker.ts
@@ -0,0 +1,4 @@
+function test() {
+    return "this";
+}
+console.log(test());
