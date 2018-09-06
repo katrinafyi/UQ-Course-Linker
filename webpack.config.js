3ee8bdfd3e0a84f0959b762084281a0241f74cfd
Kenton Lam <kenton_lam@outlook.com>
2018-09-06 11:50:51 +1000

    Import course names from json.

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = (env, argv) => {
         },
         resolve: {
             // Add '.ts' and '.tsx' as a resolvable extension.
-            extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+            extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js", '.json']
         },
         module: {
             rules: [
