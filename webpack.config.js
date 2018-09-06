cd3f127330823ff474b5be1de9b853e2752d68f6
Kenton Lam <kenton_lam@outlook.com>
2018-09-06 12:22:16 +1000

    Maintain userscript header on production.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
 let UserscriptHeaderPlugin = require('./UserscriptHeaderPlugin/UserscriptHeaderPlugin');
+let UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = (env, argv) => {
     let prod = argv.mode === 'production';
     return {
         entry: "./src/uq_course_linker.user.tsx",
         mode: (prod?"production":"development"),
+        optimization: {
+            minimizer: [
+                new UglifyJsPlugin({
+                    sourceMap: true,
+                    uglifyOptions: {
+                        output: {
+                            comments: /^ @|==/,
+                        }
+                    }
+                })
+            ]
+        },
+        devtool: prod?'source-map':'cheap-eval-source-map',
         output: {
             filename: "uq_course_linker" + (prod?".user.js":".dev.user.js")
         },
